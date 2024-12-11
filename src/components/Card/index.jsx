51e63ae1826a381ced1e22ece1bdaf9e320886e5
Ingrid Seurin <ingridseurin@gmail.com>
2024-12-11 17:31:39 +0100

    mises à jour pour deploy

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,11 +3,11 @@ import "../../styles/index.scss"
 const Card = ({ imgSrc, title, description, onCardClick, skills }) => {
   return (
     <div className="card" onClick={onCardClick}>
-      <img className="img" src={require(`../../assets/${imgSrc}`)} alt={title} />
+      <img className="img" src={`${process.env.PUBLIC_URL}/${imgSrc}`} alt={title} />
       <h3 className="noDeco">{title}</h3>
       <div className="card__logos">
         {skills.map((skill, index) => (
-          <img key={index} className="card__logo" src={require(`../../assets/${skill}`)} alt={`Logo ${index + 1}`} />
+          <img key={index} className="card__logo" src={`${process.env.PUBLIC_URL}/${skill}`} alt={`Logo ${index + 1}`} />
         ))}
       </div>
       <p className="noDeco">{description}</p>
